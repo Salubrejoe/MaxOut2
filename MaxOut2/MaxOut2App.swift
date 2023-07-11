6abb3e1f91d79cd69873bcbf98f5379ff1c039b0
Lore P <44895517+Salubrejoe@users.noreply.github.com>
2023-07-11 14:30:07 +0100

    Initial Commit

---
+++ MaxOut2/MaxOut2App.swift
@@ -0,0 +1,17 @@
+//
+//  MaxOut2App.swift
+//  MaxOut2
+//
+//  Created by Lore P on 11/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct MaxOut2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
